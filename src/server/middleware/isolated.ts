e4398cde3d3f5818f3f3e7c8bc9b0f9e9248bd22
Bailey Eaton <xYoshify@gmail.com>
2025-10-10 03:09:46 +1000

    refactor: remove KindeAuthMiddleware and handleKindeMiddleware implementations

--- src/server/middleware/isolated.ts
+++
@@ -1,6 +0,0 @@
-import { createMiddleware } from '@tanstack/react-start';
-import { handleKindeMiddleware } from './shared';
-
-export const KindeAuthMiddleware = createMiddleware().server(async ({ request, next }) => {
-  return handleKindeMiddleware(request, next);
-});
