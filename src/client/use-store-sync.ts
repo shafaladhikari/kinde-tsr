b1e210ad8636d409c61c2a2591122fae13d0189f
Bailey Eaton <xYoshify@gmail.com>
2025-10-10 03:09:23 +1000

    fix: update session handling in useSessionSync to manage session termination

--- src/client/use-store-sync.ts
+++ src/client/use-store-sync.ts
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react';
-import { fetchTokens } from '../server/fns/fetch-tokens';
+import { getSession } from '../server/fns/get-session';
 import { getClientSession } from './store';
 
 export const useSessionSync = () => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetchTokens()
+    getSession()
       .then(async (data) => {
-        await getClientSession().setItems(data);
+        const session = getClientSession();
+        if (data.message === 'SESSION_TERMINATE') {
+          await session.destroySession();
+        } else {
+          await session.setItems(data.items);
+        }
       })
       .finally(() => {
         setLoading(false);
