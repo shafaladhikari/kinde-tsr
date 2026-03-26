dee4c5e518044b31a372a95cde6b4b0ce107964c
Bailey Eaton <xyoshify@gmail.com>
2026-03-27 02:49:13 +1000

    fix: ensure we're setting `frameworkSettings` client-side

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,2 +1,8 @@
+import { frameworkSettings } from '@kinde/js-utils';
+import { version } from '../../package.json';
+
+frameworkSettings.framework = 'tanstack-start-react';
+frameworkSettings.sdkVersion = version;
+
 export { useKindeAuth } from '@kinde-oss/kinde-auth-react';
 export * from './components';
