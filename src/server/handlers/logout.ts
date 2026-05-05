59951e6c7fab90d780ff2ff32e2b4681f2aa865e
Victor Eronmosele <victoreronmosele@gmail.com>
2026-05-05 20:34:42 +0100

    fix: redirect to kinde end session endpoint on logout

--- src/server/handlers/logout.ts
+++ src/server/handlers/logout.ts
@@ -6,7 +6,11 @@ import type { KindeRouteHandler } from '../../server/types';
 export const logoutHandler: KindeRouteHandler = async (_, session) => {
   kindeLog.info('logoutHandler: firing');
   await session.destroySession();
-  throw redirect({
-    href: KindeConfig.postLogoutRedirectUrl,
-  });
+
+  const issuerUrl = KindeConfig.env.KINDE_ISSUER_URL;
+  const postLogoutRedirectUrl = KindeConfig.postLogoutRedirectUrl;
+  
+  const logoutEndpoint = `${issuerUrl}/logout?redirect=${postLogoutRedirectUrl}`;
+
+  throw redirect({ href: logoutEndpoint });
 };
